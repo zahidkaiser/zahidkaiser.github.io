b9eeeddb98d7706d9b41b8b7fc981db7d000f3cb
zahidkaiser <zahidkaiser@gmail.com>
2017-08-06 00:14:36 +0600

    email capability

--- maintainemail.php
+++ maintainemail.php
@@ -1,7 +1,7 @@
 <?php
 $to = "zahidkaiser@gmail.com";
 $subject = "Customer Query";
-$message = "Name:<br>".$_POST["name"]."<br><br>Email:<br>".$_POST["email"]."<br><br>Phone:<br>".$_POST["phone"]."<br><br>Message:<br>".$_POST["msg"];
+$message = "Name:<br>".$_POST["c_name"]."<br><br>Email:<br>".$_POST["c_email"]."<br><br>Phone:<br>".$_POST["c_phone"]."<br><br>Message:<br>".$_POST["c_message"];
 $from = $_POST["email"];
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
